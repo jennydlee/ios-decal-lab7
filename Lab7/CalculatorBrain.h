8403aa4cd350b09e43a02f41f3a30e3121fdcddc
jennydlee <jennydlee@berkeley.edu>
2017-04-20 19:29:15 -0700

    divide

--- Lab7/CalculatorBrain.h
+++ Lab7/CalculatorBrain.h
@@ -13,7 +13,8 @@ typedef enum actionType : long {
     multiply = 0,
     subtract,
     add,
-    equal
+    equal,
+    divide
 } actionType;
 @property actionType currentAction;
 @property NSString *calculationString;
--- Lab7/CalculatorBrain.m
+++ Lab7/CalculatorBrain.m
@@ -88,6 +88,8 @@
         case add:
             total = operand1 + operand2;
             break;
+        case divide:
+            total = operand1 / operand2;
         default:
             break;
     }
